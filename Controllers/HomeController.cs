7f2ceb60a24fa06094bceba18a04019d9f740802
Tanvi Trivedi <tanvvitrivedi27@gmail.com>
2024-01-23 11:39:42 +0530

    Created Cards using Partial View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,23 @@ namespace PartialViewwithASPCore.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            List<Product> products = new List<Product>()
+            { 
+                new Product() {Id=101,Name="HeadPhone",Description="Description1",Price=500.00,Image="~/Images/HeadPhone.jpg"},
+                new Product() {Id=102,Name="Camera",Description="Description2",Price=5000.00,Image="~/Images/Camera.jpg"},
+                new Product() {Id=103,Name="Shoes",Description="Description3",Price=6000.00,Image="~/Images/Shoes.jpg"},
+
+            };
+            return View(products);
+        }
+
         public IActionResult Privacy()
         {
             return View();
